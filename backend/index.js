205e2328d5493d051199652011cb1de02dcebfb9
Rohan <techhack237@gmail.com>
2024-05-08 23:29:30 +0530

    new change

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,11 @@ import path from "path"
 
 app.use(
   cors({
-    origin: "http://localhost:5173"
+    origin: "http://localhost:5173",
+    credentials: true,
   })
 );
-app.use(express.static(path.join(__dirname,"../../frontend/dist/")))
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 const bodyParser = require('body-parser');
 router.use(bodyParser.json());
@@ -19,6 +20,9 @@ app.get('/api', (req, res) => {
 });
 
 app.use('/api', router);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+});
 
 app.listen(3000, () => {
   console.log('server started on port 3000');
